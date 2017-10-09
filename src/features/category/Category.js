884597ce026947eb597b02028212a29fd66c78bb
Omar Alejandro Chacin Ortega <omar@centerattn.com>
2017-10-09 20:40:27 +0200

    restructure skeleton

--- src/features/category/Category.js
+++ src/features/category/Category.js
@@ -1,42 +1,13 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getAllCategories } from '../../actions';
 
 class Category extends Component {
-  componentDidMount() {
-    const { getAllCategories } = this.props;
-    setTimeout(() => {
-      getAllCategories();
-    });
-  }
-
-  renderCategories = () => {
-    return this.props.allCategories.map((category, index) => {
-      return <p key={index}>{category.get('name')}</p>;
-    });
-  };
-
   render() {
-    const { allCategories } = this.props;
-    if (allCategories.size === 0) {
-      return <h1>Loading...</h1>;
-    }
-
     return (
       <div>
-        <h1>Category Componentss</h1>
-        {this.renderCategories()}
+        <h1>Category Component</h1>
       </div>
     );
   }
 }
 
-function mapStateToProps({ categories }) {
-  return {
-    allCategories: categories.get('allCategories'),
-  };
-}
-
-export default connect(mapStateToProps, {
-  getAllCategories,
-})(Category);
+export default Category;
--- src/features/main/Main.js
+++ src/features/main/Main.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getAllCategories } from '../../actions';
 
 class Main extends Component {
+  componentDidMount() {
+    const { getAllCategories } = this.props;
+    setTimeout(() => {
+      getAllCategories();
+    });
+  }
+
+  renderCategories = () => {
+    return this.props.allCategories.map((category, index) => {
+      return (
+        <div key={index}>
+          <Link to={`category/${category.get('path')}`}>
+            {category.get('name')}
+          </Link>
+          <br />
+        </div>
+      );
+    });
+  };
+
   render() {
+    const { allCategories } = this.props;
+    if (allCategories.size === 0) {
+      return <h1>Loading...</h1>;
+    }
+
     return (
       <div>
-        <h1>Main Component</h1>
+        <h1>Test Componentss</h1>
+        {this.renderCategories()}
       </div>
     );
   }
 }
 
-export default Main;
+function mapStateToProps({ categories }) {
+  return {
+    allCategories: categories.get('allCategories'),
+  };
+}
+
+export default connect(mapStateToProps, {
+  getAllCategories,
+})(Main);
