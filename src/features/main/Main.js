987222ce03a15be6025937e2f89999d38fcb4dc6
Omar Alejandro Chacin Ortega <omar@centerattn.com>
2017-11-18 08:07:13 +0100

    Add table to main component

--- src/features/main/Main.js
+++ src/features/main/Main.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import moment from 'moment';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
 import { getAllCategories } from '../../actions';
 import { getAllPosts } from '../../dux/posts';
-import { Button } from 'react-bootstrap';
 
 class Main extends Component {
   componentDidMount() {
@@ -45,21 +50,69 @@ class Main extends Component {
       });
   };
 
+  formatDate = timestamp => {
+    return moment(new Date(timestamp)).format('MMM Do YY');
+  };
+
   render() {
     const { allCategories, allPosts } = this.props;
+
+    const posts = this.props.allPosts
+      .sortBy(item => item.get('voteScore'))
+      .reverse()
+      .toJS();
+
     if (allCategories.size === 0) {
       return <h1>Loading...</h1>;
     }
 
+    const data = this.props.allPosts.toJS();
+
+    const columns = [
+      {
+        Header: 'Title',
+        accessor: 'title', // String-based value accessors!
+      },
+      {
+        Header: 'Author',
+        accessor: 'author',
+      },
+      {
+        Header: 'Category',
+        accessor: 'category',
+      },
+      {
+        Header: 'Vote',
+        accessor: 'voteScore',
+      },
+      {
+        Header: 'Created',
+        accessor: 'timestamp',
+        Cell: props => (
+          <span className="number">{this.formatDate(props.value)}</span>
+        ),
+      },
+      {
+        Header: '',
+        accessor: '',
+        Cell: props => (
+          <Link to={'/createPost/'}>
+            <Button bsStyle="primary">GO</Button>
+          </Link>
+        ),
+      },
+    ];
+
     return (
       <div>
-        <h1>Test Componentss</h1>
-
         <Link to={'/createPost/'}>
           <Button bsStyle="primary">Create new post</Button>
         </Link>
+        <ReactTable data={data} columns={columns} />
 
+        <h3>Categories</h3>
         {this.renderCategories()}
+        <h3>Votes</h3>
         {this.renderPosts()}
       </div>
     );
